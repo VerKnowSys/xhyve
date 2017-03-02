75bde0635a8eb0707b88fef9e5b664f523e87a4d
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-03-02 07:04:36 +0100

    new: Improved bin/boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,27 +1,28 @@
 #!/bin/sh
 
-# Linux
-# KERNEL="test/vmlinuz"
-# INITRD="test/initrd.gz"
-# CMDLINE="earlyprintk=serial console=ttyS0"
-
-# FreeBSD
 USERBOOT="lib/userboot.so"
-BOOTVOLUME="/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
-KERNELENV="-v"
+KERNELENV=""
+
+# FreeBSD installer disk:
+HDD0="/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
+
+# FreeBSD data disk:
+HDD1="/Studio/VMs/xhyve.disks/xhyve1.dmg"
+
+IMG_HDD0="-s 4:0,ahci-hd,${HDD0}"
+IMG_HDD1="-s 4:1,ahci-hd,${HDD1}"
+
+BOOTVOLUME="${HDD1}"
 
 MEM="-m 4G"
 SMP="-c 4"
-# NET="-s 2:0,virtio-net"
-# IMG_CD="-s 3,ahci-cd,/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
-IMG_HDD="-s 4,virtio-blk,/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
+NET="-s 2:0,virtio-net"
 PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
 LPC_DEV="-l com1,stdio"
 ACPI="-A"
 UUID="-U 13725C2F-FF66-4F9D-AD7F-D3FC94FBF40F"
 
-# Linux
-# build/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD $UUID -f kexec,$KERNEL,$INITRD,"$CMDLINE"
+# start HardenedBSD:
+sudo bin/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD0 $IMG_HDD1 $UUID -f fbsd,$USERBOOT,$BOOTVOLUME,"$KERNELENV"
 
-# FreeBSD
-bin/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD $UUID -f fbsd,$USERBOOT,$BOOTVOLUME,"$KERNELENV"
+# NOTE: sudo is necessary for NET virtio-net to work :()
