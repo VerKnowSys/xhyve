1a0799c8612c7b3476dee585a7318bbecbd5fd83
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-03-02 17:27:13 +0100

    new: Cleanup and refactor of scripts. Numerous improvements.

--- bin/boot.sh
+++ bin/boot.sh
@@ -4,25 +4,43 @@ USERBOOT="lib/userboot.so"
 KERNELENV=""
 
 # FreeBSD installer disk:
-HDD0="/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
+INSTALLER_DISK_IMAGE="/Data/ISO/HardenedBSD-11-STABLE-v46.15-amd64-memstick.img"
+BASE_DISK_IMAGE=""
 
 # FreeBSD data disk:
+#
+# > zfs create -sV 50g Studio/VMs/xhyve-1.zvol
+# > ioreg -trn "ZVOL Studio/VMs/xhyve-1.zvol Media" | grep "BSD Name" to find out assigned /dev/[r]diskN
 HDD1="/dev/rdisk7"
 
-IMG_HDD0="-s 4:0,ahci-hd,${HDD0}"
+IMG_HDD0="-s 4:0,ahci-hd,${INSTALLER_DISK_IMAGE}"
 IMG_HDD1="-s 4:1,ahci-hd,${HDD1}"
 
 BOOTVOLUME="${HDD1}"
 
-MEM="-m 4G"
+UUID="-U 13725C2F-FF66-4F9D-AD7F-D3FC94FBF40F"
+MEM="-m 6G"
 SMP="-c 4"
 NET="-s 2:0,virtio-net"
-PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
-LPC_DEV="-l com1,stdio"
+PCI_DEV="-s 0:0,hostbridge"
+LPC_DEV="-s 31,lpc -l com1,stdio"
 ACPI="-A"
-UUID="-U 13725C2F-FF66-4F9D-AD7F-D3FC94FBF40F"
+OPTIONS="-w -x -W -H"
 
+#
 # start HardenedBSD:
-sudo bin/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD0 $IMG_HDD1 $UUID -f fbsd,$USERBOOT,$BOOTVOLUME,"$KERNELENV"
+# NOTE: sudo is necessary only for NET virtio-net to work :()
+sudo bin/xhyve \
+    ${UUID} \
+    ${ACPI} \
+    ${MEM} \
+    ${SMP} \
+    ${PCI_DEV} \
+    ${LPC_DEV} \
+    ${NET} \
+    ${IMG_CD} \
+    ${IMG_HDD0} \
+    ${IMG_HDD1} \
+    ${OPTIONS} \
+    -f fbsd,${USERBOOT},${BOOTVOLUME},"${KERNELENV}"
 
-# NOTE: sudo is necessary for NET virtio-net to work :()
--- bin/build-xhyve.sh
+++ bin/build-xhyve.sh
@@ -1,13 +1,15 @@
 #£/bin/sh
 
-reset
-clear
-_timestamp="$(date +%F-%s)"
+
 _origin="https://github.com/"
 _suffix="xhyve.git"
+_userboot="userboot.so"
+_timestamp="$(date +%F-%s)"
+
+reset
 
 printf "Cleaning old src…\n"
-rm -vf bin/xhyve lib/userboot.so
+rm -vf bin/xhyve lib/${_userboot}
 rm -rf src
 mkdir -p lib
 
@@ -17,16 +19,15 @@ git clone -q "${_origin}mist64/${_suffix}" src
 cd src
 
 for _repo_pr in dborca pr1ntf zchee dwoz bonifaido; do
-    printf "\n\nFetching changes from: ${_origin}${_repo_pr}/${_suffix}\n"
     set -e
-    git pull --no-verify --force --squash --strategy recursive "${_origin}${_repo_pr}/${_suffix}"
+    git pull --no-verify --force --squash --strategy recursive "${_origin}${_repo_pr}/${_suffix}" && \
+      printf "\nPulled changes from repository: ${_origin}${_repo_pr}/${_suffix}\n"
 
     set +e
     git add --all src/*
     echo "Commited changeset from repository: ${_repo_pr} @ ${_timestamp}" > /tmp/cmtmsg1
     git commit --all --no-verify --file /tmp/cmtmsg1
 
-    set +e
     git status 2>/dev/null | egrep '^nothing to commit, working tree clean' >/dev/null 2>&1
     if [ "0" != "${?}" ]; then
         set -e
@@ -39,22 +40,18 @@ done
 
 
 printf "\n\nConfiguring hardened compiler options for xhyve…\n"
-sed -i '' -e "s|-g|\$(CFLAGS_SECURITY)|; s|-Os|-O3|;" config.mk
+sed -i '' -e "s|-g|\$(CFLAGS_SECURITY)|;" config.mk
 
 set -e
 printf "\nCFLAGS_SECURITY := \\
-  -arch x86_64 \\
   -mmacosx-version-min=10.11 \\
   -ftrapv \\
   -fstack-protector \\
-  -fstack-protector-strong \\
-  -fstack-protector-all \\
   --param ssp-buffer-size=4 \\
   -fno-strict-overflow \\
   -D_FORTIFY_SOURCE=2 \\
   -Wformat \\
-  -Wformat-security \\
-  -fPIC \
+  -Wformat-security \
 " >> config.mk
 set +e
 sed -i '' -e 's|en_US.US-ASCII|en_GB.UTF-8|;' config.mk
@@ -63,41 +60,8 @@ set -e
 printf "\n\nBuilding xhyve…\n"
 V=1 make all
 
-printf "\n\nCopying userboot.so and boot files…\n"
-cd build
-
-cp -vfr ../test/* ../../lib/
-
-# clang -arch x86_64 \
-#     -dead_strip \
-#     -shared \
-#     -fno-common \
-#     -fvisibility=hidden \
-#     -fstrict-aliasing \
-#     -framework vmnet \
-#     -framework Hypervisor \
-#     -o ../../lib/userboot.so \
-#     vmm/*.o vmm/io/*.o vdsk/*.o vmm/intel/*.o firmware/*.o xhyve.lto.o
-
-# clang -o \
-#     ../../lib/userboot.so \
-#     -flto \
-#     -target x86_64-apple-darwin14 \
-#     -mmacosx-version-min=10.11 \
-#     -arch x86_64 \
-#     -dead_strip \
-#     -fstrict-aliasing \
-#     -shared \
-#     -std=c11 \
-#     -fno-common \
-#     -fvisibility=hidden \
-#     -framework vmnet \
-#     -framework Hypervisor \
-#     -framework System \
-#     -Xlinker \
-#     -object_path_lto \
-#     vmm/*.o vmm/io/*.o vdsk/*.o vmm/intel/*.o firmware/*.o xhyve.lto.o
-cd ../
+printf "\n\nInstalling ${_userboot}…\n"
+install -v test/${_userboot} ../lib/
 
 printf "\n\nInstalling xhyve…\n"
 install -v build/xhyve ../bin/xhyve
