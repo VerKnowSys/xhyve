33ec2e6c3b5d5e8d5d65d7ba6b8d6dff86d43231
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-03-09 21:03:47 +0100

    new: Perform origin snapshot after invoking bin/boot_installer.sh

--- bin/boot_installer.sh
+++ bin/boot_installer.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+DEFAULT_ORIGIN="Studio/VMs/xhyve-1.zvol@origin"
+
+
 bin/shutdown
 
 echo "Enabling installer image for boot"
@@ -15,6 +19,12 @@ sed -i '' \
     -e 's|BOOTVOLUME="${INSTALLER_DISK_IMAGE}"|BOOTVOLUME="${HDD1}"|g;' \
     bin/boot.sh \
     2>/dev/null
+
+zfs list -t snap "${DEFAULT_ORIGIN}" >/dev/null 2>&1
+if [ "${?}" != "0" ]; then
+    echo "Creating 'origin' snapshot: ${DEFAULT_ORIGIN}, after installation process."
+    zfs snapshot "${DEFAULT_ORIGIN}"
+fi
 
 echo
 echo "Run bin/boot_continuous.sh to boot default environment"
