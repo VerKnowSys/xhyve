ba48251d644a5f83226441f656e8c4b56477b885
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-03-09 21:09:56 +0100

    new: Implemented file triggered shutdown for bin/boot_continuous.sh

--- bin/boot_continuous.sh
+++ bin/boot_continuous.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+check_nostart () {
+    if [ -f ".nostart" ]; then
+        rm -f ".nostart"
+        echo "Requested bin/shutdown, continuous mode stopped."
+        exit
+    fi
+}
+
 while true; do
     clear
     reset
     echo "Booting xHyve…"
     bin/boot.sh
     echo "Shutdown completed"
+    check_nostart
 done
 exit
