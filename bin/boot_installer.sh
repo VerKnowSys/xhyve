99cb176a9894abc06c7fa0de5fcf236d8f9857a1
Daniel (dmilith) Dettlaff <dmilith@me.com>
2017-03-31 16:42:26 +0200

    new: Updated bin/boot_installer.sh after recent bin/boot.sh refactor. More in details.
    * Since HDD1 is now VD_STABLE or VD_CURRENT, sed replacements has to replace according values properly
    * Removed DEFAULT_ORIGIN and ZFS snapshot creation after bin/boot_installer.sh finishes.

--- bin/boot_installer.sh
+++ bin/boot_installer.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
 
 
-DEFAULT_ORIGIN="Studio/VMs/xhyve-1.zvol@origin"
-
-
 bin/shutdown
 
 echo "Enabling installer image for boot"
 sed -i '' \
-    -e 's|BOOTVOLUME="${HDD1}"|BOOTVOLUME="${INSTALLER_DISK_IMAGE}"|g;' \
-    bin/boot.sh \
-    2>/dev/null
+    -e 's|BOOTVOLUME="${VD_CURRENT}"|BOOTVOLUME="${INSTALLER_CURRENT}"|g; s|BOOTVOLUME="${VD_STABLE}"|BOOTVOLUME="${INSTALLER_STABLE}"|g;' \
+    bin/boot.sh 2>/dev/null
 
 bin/boot.sh
 
-echo "Switch default boot device to 'HDD1'"
+echo "Switch to default boot device"
 sed -i '' \
-    -e 's|BOOTVOLUME="${INSTALLER_DISK_IMAGE}"|BOOTVOLUME="${HDD1}"|g;' \
-    bin/boot.sh \
-    2>/dev/null
-
-zfs list -t snap "${DEFAULT_ORIGIN}" >/dev/null 2>&1
-if [ "${?}" != "0" ]; then
-    echo "Creating 'origin' snapshot: ${DEFAULT_ORIGIN}, after installation process."
-    zfs snapshot "${DEFAULT_ORIGIN}"
-fi
+    -e 's|BOOTVOLUME="${INSTALLER_CURRENT}"|BOOTVOLUME="${VD_CURRENT}"|g; s|BOOTVOLUME="${INSTALLER_STABLE}"|BOOTVOLUME="${VD_STABLE}"|g;' \
+    bin/boot.sh 2>/dev/null
 
 echo
 echo "Run bin/boot_continuous.sh to boot default environment"
